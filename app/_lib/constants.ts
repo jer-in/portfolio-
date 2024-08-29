73402345c2883e98a1e99bc344762f6bb09dc659
andrijaweb <andrijawebd@gmail.com>
2024-08-29 21:22:12 +0200

    Added constants.ts file in lib

---
+++ app/_lib/constants.ts
@@ -0,0 +1,38 @@
+export const techCardsItems = [
+  {
+    name: "Figma",
+    description: "Design Tool",
+    imageUrl: "/imgs/logos/figma-logo.svg",
+    bgColor: "bg-[#0ACF83]/20",
+  },
+  {
+    name: "TypeScript",
+    description: "JavaScript but better",
+    imageUrl: "/imgs/logos/typescript.svg",
+    bgColor: "bg-[#3178C6]/20",
+  },
+  {
+    name: "React",
+    description: "JavaScript Library",
+    imageUrl: "/imgs/logos/react.svg",
+    bgColor: "bg-[#61DAFB]/20",
+  },
+  {
+    name: "NextJS",
+    description: "React framework",
+    imageUrl: "/imgs/logos/nextjs.svg",
+    bgColor: "bg-white/15",
+  },
+  {
+    name: "Tailwind",
+    description: "CSS framework",
+    imageUrl: "/imgs/logos/tailwind.svg",
+    bgColor: "bg-[#0EA5E9]/20",
+  },
+  {
+    name: "Git",
+    description: "Version control",
+    imageUrl: "/imgs/logos/git.svg",
+    bgColor: "bg-[#F1502F]/20",
+  },
+];
