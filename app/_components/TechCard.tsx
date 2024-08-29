69d138641fdb46b856e27673d4323535e1b26cf7
andrijaweb <andrijawebd@gmail.com>
2024-08-29 21:32:02 +0200

    Added dark color variation to tailwind config, added hover effect to tech card

--- app/_components/TechCard.tsx
+++ app/_components/TechCard.tsx
@@ -13,7 +13,7 @@ const TechCard = ({
   const { name, description, imageUrl, bgColor } = cardInfo;
 
   return (
-    <div className="flex flex-1 gap-5 p-2.5 rounded-xl bg-dark-200">
+    <div className="flex flex-1 gap-5 p-2.5 rounded-xl border border-dark-200 bg-dark-200 hover:bg-dark-300 hover:border-dark-700 transition-colors duration-200">
       <div className={`p-3 ${bgColor} rounded-lg w-fit`}>
         <Image
           src={imageUrl}
