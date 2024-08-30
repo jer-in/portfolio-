1dd78b5e28a8d296cede61c9720c35b708b37677
andrijaweb <andrijawebd@gmail.com>
2024-08-30 20:05:59 +0200

    Added style for light version

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -5,7 +5,7 @@ import { AnimatedTooltip } from "./ui/Tooltip";
 const HeroSection = () => {
   return (
     <>
-      <div className="h-screen w-full dark:bg-dark-100 bg-white dark:bg-grid-white/[0.04] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-dark-100 bg-white dark:bg-grid-white/[0.04] bg-grid-black/[0.06] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-dark-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
@@ -14,15 +14,15 @@ const HeroSection = () => {
         className="pb-20 pt-36 sm:p-0 sm:min-h-screen flex flex-col items-center justify-center relative"
         id="#home"
       >
-        <div className="flex flex-col justify-center items-center relative z-10 text-center">
+        <div className="flex flex-col justify-center items-center relative z-10 text-center text-dark-200 dark:text-stone-200">
           <p className="uppercase font-bold text-sm tracking-widest">
             Based In Serbia
           </p>
-          <h1 className="text-[40px] md:text-6xl lg:text-7xl font-bold text-center max-w-5xl leading-tight tracking-wide text-stone-200">
+          <h1 className="text-[40px] md:text-6xl lg:text-7xl font-bold text-center max-w-5xl leading-tight tracking-wide">
             Quality Design &{" "}
             <span className="text-primary">Web Development</span> Synergy
           </h1>
-          <p className="pt-5 pb-8 text-sm sm:text-base md:text-lg text-white/70">
+          <p className="pt-5 pb-8 text-sm sm:text-base md:text-lg text-dark-200 dark:text-stone-200/70">
             Hi, I&apos;m Andrija, I create intuitive, visually stunning and
             highly functional web applications.
           </p>
--- app/_components/ui/Navbar.tsx
+++ app/_components/ui/Navbar.tsx
@@ -56,7 +56,7 @@ export const Navbar = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-dark-700 rounded-lg dark:bg-dark-200 bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-4  items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-dark-700 rounded-lg bg-dark-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-4  items-center justify-center space-x-4",
           className
         )}
       >
@@ -65,7 +65,7 @@ export const Navbar = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative text-neutral-50 items-center flex space-x-1 hover:text-neutral-300"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
