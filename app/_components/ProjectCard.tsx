74e76b051cec1a87092cc376a84b30fa4b78aafe
andrijaweb <andrijawebd@gmail.com>
2024-08-31 23:59:57 +0200

    Added real data to tech stack component

--- app/_components/ProjectCard.tsx
+++ app/_components/ProjectCard.tsx
@@ -6,10 +6,17 @@ interface ProjectProps {
   heading: string;
   description: string;
   imageUrl: string;
+  techStack: string[];
 }
 
 const ProjectCard = ({ project }: { project: ProjectProps }) => {
-  const { position = "left", heading, description, imageUrl } = project;
+  const {
+    position = "left",
+    heading,
+    description,
+    imageUrl,
+    techStack,
+  } = project;
 
   return (
     <div
@@ -22,7 +29,7 @@ const ProjectCard = ({ project }: { project: ProjectProps }) => {
       >
         <h3 className="text-3xl font-semibold">{heading}</h3>
         <p className="text-stone-200/80 leading-7">{description}</p>
-        <ProjectTechnologiesMini />
+        <ProjectTechnologiesMini techStack={techStack} />
         <div className="mt-auto">
           <button className="w-full text-center border border-dark-400 hover:bg-dark-300 transition-colors duration-200 py-2.5 rounded-full font-medium">
             Source Code
--- app/_components/ProjectTechnologiesMini.tsx
+++ app/_components/ProjectTechnologiesMini.tsx
@@ -1,18 +1,14 @@
-const ProjectTechnologiesMini = () => {
+const ProjectTechnologiesMini = ({ techStack }: { techStack: string[] }) => {
   return (
     <div className="flex flex-wrap items-center gap-2.5">
-      <p className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4">
-        React
-      </p>
-      <p className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4">
-        TypeScript
-      </p>
-      <p className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4">
-        TailwindCSS
-      </p>
-      <p className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4">
-        Appwrite
-      </p>
+      {techStack.map((tech) => (
+        <p
+          key={tech}
+          className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4"
+        >
+          {tech}
+        </p>
+      ))}
     </div>
   );
 };
