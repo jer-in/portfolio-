2c99ec106d8af4326b9de8f09802ff450287555e
andrijaweb <andrijawebd@gmail.com>
2024-08-31 23:57:53 +0200

    Added project technologies component

---
+++ app/_components/ProjectTechnologiesMini.tsx
@@ -0,0 +1,20 @@
+const ProjectTechnologiesMini = () => {
+  return (
+    <div className="flex flex-wrap items-center gap-2.5">
+      <p className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4">
+        React
+      </p>
+      <p className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4">
+        TypeScript
+      </p>
+      <p className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4">
+        TailwindCSS
+      </p>
+      <p className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4">
+        Appwrite
+      </p>
+    </div>
+  );
+};
+
+export default ProjectTechnologiesMini;
