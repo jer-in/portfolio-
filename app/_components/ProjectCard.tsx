84b96289a41142e3813ffab430d105c102f01783
andrijaweb <andrijawebd@gmail.com>
2024-09-01 10:19:39 +0200

    Light mode style

--- app/_components/ProjectCard.tsx
+++ app/_components/ProjectCard.tsx
@@ -20,7 +20,7 @@ const ProjectCard = ({ project }: { project: ProjectProps }) => {
 
   return (
     <div
-      className={`p-5 rounded-xl border border-dark-400 grid ${
+      className={`p-5 rounded-xl border border-dark-700/20 dark:border-dark-400 grid ${
         position === "right"
           ? "lg:grid-cols-[3fr_1fr]"
           : "lg:grid-cols-[1.5fr_3fr]"
@@ -34,24 +34,24 @@ const ProjectCard = ({ project }: { project: ProjectProps }) => {
         <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold">
           {heading}
         </h3>
-        <p className="text-sm md:text-base text-stone-200/80 leading-5 md:leading-7">
+        <p className="text-sm md:text-base dark:text-stone-200/80 leading-5 md:leading-7">
           {description}
         </p>
         <ProjectTechnologiesMini techStack={techStack} />
         <div className="mt-auto">
-          <button className="w-full text-center text-sm sm:text-base border border-dark-400 hover:bg-dark-300 transition-colors duration-200 py-2 sm:py-2.5 rounded-full font-medium">
+          <button className="w-full text-center hover:text-stone-200 text-sm sm:text-base border border-dark-400 hover:bg-dark-300 transition-colors duration-200 py-2 sm:py-2.5 rounded-full font-medium">
             Source Code
           </button>
         </div>
       </div>
 
-      <div className="relative rounded-lg overflow-hidden">
+      <div className="rounded-lg overflow-hidden">
         <Image
           src={imageUrl}
           width={1000}
           height={1000}
           alt="portfolio showcase"
-          className="object-cover w-full h-full hover:scale-110 transition-transform duration-700 cursor-pointer"
+          className="hover:scale-110 transition-transform duration-700 cursor-pointer"
         />
       </div>
     </div>
--- app/_components/ProjectTechnologiesMini.tsx
+++ app/_components/ProjectTechnologiesMini.tsx
@@ -4,7 +4,7 @@ const ProjectTechnologiesMini = ({ techStack }: { techStack: string[] }) => {
       {techStack.map((tech) => (
         <p
           key={tech}
-          className="text-xs font-bold hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4"
+          className="text-xs font-bold hover:bg-dark-300/10 dark:hover:bg-dark-300 border border-dark-400 transition-colors duration-200 w-fit rounded-md py-2 px-4"
         >
           {tech}
         </p>
