f2318b1f8af69892aa02807166384ff44d07c48b
andrijaweb <andrijawebd@gmail.com>
2024-09-10 20:46:59 +0200

    Added dynamic page for single project, ShinyButton small style fix

---
+++ app/work/[projectName]/page.tsx
@@ -0,0 +1,97 @@
+import Footer from "@/app/_components/Footer";
+import ProjectTechnologiesMini from "@/app/_components/ProjectTechnologiesMini";
+import { Navbar } from "@/app/_components/ui/Navbar";
+import ShinyButton from "@/app/_components/ui/ShinyButton";
+import { portfolioProjects } from "@/app/_lib/constants";
+import {
+  BriefcaseBusiness,
+  Code,
+  ContactIcon,
+  Globe,
+  House,
+  MoveDown,
+  UserRound,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+const navItems = [
+  { name: "Home", link: "/", icon: <House /> },
+  { name: "Work", link: "/#work", icon: <BriefcaseBusiness /> },
+  { name: "About", link: "/#about", icon: <UserRound /> },
+  { name: "Contact", link: "/#contact", icon: <ContactIcon /> },
+];
+
+const ProjectOverview = ({ params }: { params: { projectName: string } }) => {
+  const projectId = params.projectName;
+  const project = portfolioProjects.find((project) => project.id === projectId);
+
+  if (!project) return notFound();
+
+  const {
+    heading,
+    subheading,
+    description,
+    imageUrl,
+    techStack,
+    liveDemoUrl,
+    sourceCodeUrl,
+  } = project;
+
+  return (
+    <main className="flex flex-col px-5 sm:px-10 relative">
+      <div className="max-w-7xl mx-auto w-full">
+        <Navbar navItems={navItems} />
+
+        <div className="pt-36">
+          <h1 className="text-[40px] md:text-6xl lg:text-7xl text-center max-w-5xl leading-normal tracking-wide uppercase mx-auto">
+            <span className="font-black">{heading}</span>
+            <br />
+            {subheading}
+          </h1>
+
+          <div className="flex items-center justify-center my-24">
+            <Link href="#image">
+              <MoveDown className="size-16" strokeWidth={1} />
+            </Link>
+          </div>
+
+          <div className="rounded-lg overflow-hidden" id="image">
+            <Image src={imageUrl} width={2000} height={1000} alt="portfolio" />
+          </div>
+
+          <div className="mt-8 mb-32 flex justify-between">
+            <div className="flex-1">
+              <h2 className="text-3xl min-[430px]:text-4xl md:text-5xl dark:text-stone-200 mb-5">
+                Project Overview
+              </h2>
+
+              <ProjectTechnologiesMini techStack={techStack} />
+
+              <div className="flex items-center gap-4 mt-10">
+                <ShinyButton icon={<Globe />} iconPosition="left">
+                  <Link href={liveDemoUrl} target="_blank">
+                    View Demo
+                  </Link>
+                </ShinyButton>
+
+                <ShinyButton icon={<Code />} iconPosition="left">
+                  <Link href={sourceCodeUrl} target="_blank">
+                    Source Code
+                  </Link>
+                </ShinyButton>
+              </div>
+            </div>
+
+            <p className="flex-1">{description}</p>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default ProjectOverview;
